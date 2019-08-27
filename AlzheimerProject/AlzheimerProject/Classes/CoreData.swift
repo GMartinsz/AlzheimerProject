e6c923e35e15d2b688face845ff96c3bf385ffb6
Pedro Paulo <Pedro.feitosa.carneiro@gmail.com>
2019-08-27 15:48:10 -0300

    branch CoreData criada e arquivo do core data inserido no projeto

---
+++ AlzheimerProject/AlzheimerProject/Classes/CoreData.swift
@@ -0,0 +1,31 @@
+//
+//  CoreData.swift
+//  AlzheimerProject
+//
+//  Created by Pedro Paulo Feitosa Rodrigues Carneiro on 27/08/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let managedObjectContext = appDelegate.persistentContainer.viewContext
+
+class CoreDataBase {
+    
+    private init(){
+        
+    }
+    
+    static var coreData = CoreDataBase()
+    
+    //Função responsável por criar a sala!
+    static func createSala(){
+        
+    }
+    
+    
+    
+}
